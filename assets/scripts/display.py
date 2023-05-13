ff66f54e8e9d7c64ab6341ad8557633ac6f47462
Marcello Muylaert <marcellomuylaert@icloud.com>
2023-05-13 14:37:17 +0100

    Refactor game.py main function, create Main class. Create display.py and Display class

---
+++ assets/scripts/display.py
@@ -0,0 +1,36 @@
+'''Holds the display functionality'''
+import pygame
+from pygame import mixer
+
+mixer.init()
+
+class Display:
+    '''A class that handles display related actions'''
+    def __init__(self, width, height, caption):
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption(caption)
+
+    def load_image(self, path, size=None):
+        '''load and scale background image'''
+        image = pygame.image.load(path).convert_alpha()
+        if size:
+            image = pygame.transform.scale(image, size)
+        return image
+
+    def draw_background(self, image):
+        '''Draw background'''
+        self.screen.blit(image, (0, 0))
+
+    def draw_sprite(self, sprite):
+        '''Draw sprite'''
+        sprite.draw(self.screen)
+
+    def draw_health_bar(self, bar):
+        '''Draw health bar'''
+        bar.draw(self.screen)
+
+    def update(self):
+        '''Update display'''
+        pygame.display.flip()
